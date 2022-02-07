76c408a005124a7c50a346318e5a9f341dce575f
Saule <sauleojcieva225@gmail.com>
2022-02-07 22:00:45 +0600

    add filter input to main page

--- src/common/components/Main-page/index.jsx
+++ src/common/components/Main-page/index.jsx
@@ -1,14 +1,14 @@
 import './index.scss'
 import Banner from "../Banner-main";
 import HelloText from "../Hello-text";
-// import SearchInput from '../Search-input';
+import SearchInput from '../Search-input';
 
 const MainPage = () => {
   return (
     <div className='container'>
       <HelloText />
       <Banner />
-      {/* <SearchInput /> */}
+      <SearchInput />
     </div>
   )
 }
---
+++ src/common/components/Search-input/index.jsx
@@ -0,0 +1,11 @@
+import './index.scss';
+
+const SearchInput = () => {
+  return (
+    <div>
+      <input  className='search_input ' placeholder='Поиск по названию'/>
+    </div>
+  )
+}
+
+export default SearchInput;

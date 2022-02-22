8bf37cbcc19a61ccac2eacb6238a9d54439cc509
Saule <sauleojcieva225@gmail.com>
2022-02-23 00:51:21 +0600

    Added mixins,import some styles to global

--- src/common/components/Footer-menu/index.jsx
+++ src/common/components/Footer-menu/index.jsx
@@ -1,5 +1,5 @@
 import CatalogButton from '../Catalog-button';
-import HomeButton from '../Home-button';
+import HomeButton from '../home-button';
 import PersonalButton from '../Personal-area-button';
 import ShoppingButton from '../Shopping-button';
 import './index.scss';

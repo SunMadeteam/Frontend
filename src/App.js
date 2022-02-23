2ad3ee3b19ba8c8de75ef233c9f463e8bd5d7d20
Saule <sauleojcieva225@gmail.com>
2022-02-23 15:44:10 +0600

    Added category page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MainPage from './features/Main-page';
 import Registration from './features/Registration';
 import RegistrationConfirmationCode from './features/Registration-confirmation-code';
 import Authorization from './features/Authorization';
+import Catalog from './features/Catalogs';
 
 
 function App() {
@@ -13,11 +14,12 @@ function App() {
         <Route path='/' element={<MainPage/>} />
         <Route path='/personal' element={<Registration/>} />
         <Route path='confirmation' element={<RegistrationConfirmationCode />} />
-        <Route path='/auth'element={<Authorization/>} />
+        <Route path='/auth' element={<Authorization/>} />
+        <Route path='/catalogs' element={<Catalog />} />
+        <Route path='/shopping' element={<Catalog />} />
       </Routes>
       <FooterMenu />
-      <hr/>
-      
+      <hr />      
     </div>
   );
 }

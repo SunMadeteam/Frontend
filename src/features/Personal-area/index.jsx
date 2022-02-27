c033387e94ce49f93838b85e4e587f3dbb5010fb
Saule <sauleojcieva225@gmail.com>
2022-02-27 18:49:23 +0600

    Added Input component to Personal-area page

--- src/features/Personal-area/index.jsx
+++ src/features/Personal-area/index.jsx
@@ -1,11 +1,14 @@
 import './index.scss';
 import right from './img/right.svg';
 import HelloText from './../../common/components/Hello-text/index';
+import Input from './../../common/components/Input/Input';
 
 const PersonalArea = () => {
   return (
     <div className = 'personal_area container'>
       <HelloText />
+      <Input type='text' placeholder ='Имя'/>
+      <Input type='number' placeholder ='Телефон'/>
       <div className='personal_buttons'>
         <div className='history'>
           <p>История заказов</p>

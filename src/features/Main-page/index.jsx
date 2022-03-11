77cb1f190a06d0ffee110857a8ec7d8039b0017e
Saule <sauleojcieva225@gmail.com>
2022-03-11 22:40:41 +0600

    Deleted cart from Main page

--- src/features/Main-page/index.jsx
+++ src/features/Main-page/index.jsx
@@ -12,9 +12,6 @@ const MainPage = () => {
       <Banner />
       <CategoryTitle title="Популярные" />
       <AllProductCarts />
-      <AllProductCarts />
-      <AllProductCarts />
-      <AllProductCarts />
       <hr />
     </div>
   )

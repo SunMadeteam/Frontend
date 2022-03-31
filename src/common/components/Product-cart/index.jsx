f368433bf6e01b98fe7d1e434f15255309dece5f
Saule <sauleojcieva225@gmail.com>
2022-03-31 15:30:37 +0600

    Fixed a bug with displaying a detailed description of the product card

--- src/common/components/Product-cart/index.jsx
+++ src/common/components/Product-cart/index.jsx
@@ -4,17 +4,17 @@ import PlusMinus from './../Plus-minus-button/index';
 import { Link } from 'react-router-dom';
 
 const ProductCart = ({base}) => {
-  console.log(base)
+  // console.log(base)
   return (
     <div className='product_cart'>
       <div className='cart_img'>
-        <Link to ={'/cartDescription/' + base.productId}>
+        <Link to ={'/cartDescription/' + base.id}>
           <img src={base.image} alt="" /> 
         </Link>
         <FavoriteHeart />
       </div>
       <div className='cart_body'>
-        <Link to ={'/cartDescription/' + base.productId}>
+        <Link to ={'/cartDescription/' + base.id}>
             <p className='title'>{base.name}</p>
         </Link> 
         <div className='price_wrap flex'>
